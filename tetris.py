2e753d54094ee646bedf00b09fe6ad095d9717ac
Safaraliyev05 <safaraliyevsardor05@gmail.com>
2025-01-11 16:19:50 +0500

    message

--- tetris.py
+++ tetris.py
@@ -1,10 +1,8 @@
-import pygame
 import random
 
-
+import pygame
 
 pygame.font.init()
-
 
 s_width = 800
 s_height = 700
@@ -14,9 +12,6 @@ block_size = 30
 
 top_left_x = (s_width - play_width) // 2
 top_left_y = s_height - play_height
-
-
-
 
 S = [['.....',
       '.....',
@@ -124,7 +119,6 @@ shapes = [S, Z, I, O, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
 
 
-
 class Piece(object):
     rows = 20
     columns = 10
@@ -137,13 +131,13 @@ class Piece(object):
         self.rotation = 0
 
 
-def create_grid(locked_positions={}):
-    grid = [[(0,0,0) for x in range(10)] for x in range(20)]
+def create_grid(locked_positions):
+    grid = [[(0, 0, 0) for x in range(10)] for x in range(20)]
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if (j,i) in locked_positions:
-                c = locked_positions[(j,i)]
+            if (j, i) in locked_positions:
+                c = locked_positions[(j, i)]
                 grid[i][j] = c
     return grid
 
@@ -165,7 +159,7 @@ def convert_shape_format(shape):
 
 
 def valid_space(shape, grid):
-    accepted_positions = [[(j, i) for j in range(10) if grid[i][j] == (0,0,0)] for i in range(20)]
+    accepted_positions = [[(j, i) for j in range(10) if grid[i][j] == (0, 0, 0)] for i in range(20)]
     accepted_positions = [j for sub in accepted_positions for j in sub]
     formatted = convert_shape_format(shape)
 
@@ -195,16 +189,17 @@ def draw_text_middle(text, size, color, surface):
     font = pygame.font.SysFont('comicsans', size, bold=True)
     label = font.render(text, 1, color)
 
-    surface.blit(label, (top_left_x + play_width/2 - (label.get_width() / 2), top_left_y + play_height/2 - label.get_height()/2))
+    surface.blit(label, (
+        top_left_x + play_width / 2 - (label.get_width() / 2), top_left_y + play_height / 2 - label.get_height() / 2))
 
 
 def draw_grid(surface, row, col):
     sx = top_left_x
     sy = top_left_y
     for i in range(row):
-        pygame.draw.line(surface, (128,128,128), (sx, sy+ i*30), (sx + play_width, sy + i * 30))
+        pygame.draw.line(surface, (128, 128, 128), (sx, sy + i * 30), (sx + play_width, sy + i * 30))
         for j in range(col):
-            pygame.draw.line(surface, (128,128,128), (sx + j * 30, sy), (sx + j * 30, sy + play_height))
+            pygame.draw.line(surface, (128, 128, 128), (sx + j * 30, sy), (sx + j * 30, sy + play_height))
 
 
 def clear_rows(grid, locked):
@@ -339,7 +334,6 @@ def main():
             next_piece = get_shape()
             change_piece = False
 
-
             if clear_rows(grid, locked_positions):
                 score += 10
 
@@ -351,7 +345,7 @@ def main():
         if check_lost(locked_positions):
             run = False
 
-    draw_text_middle("You Lost", 40, (255,255,255), win)
+    draw_text_middle("You Lost", 40, (255, 255, 255), win)
     pygame.display.update()
     pygame.time.delay(2000)
 
@@ -359,7 +353,7 @@ def main():
 def main_menu():
     run = True
     while run:
-        win.fill((0,0,0))
+        win.fill((0, 0, 0))
         draw_text_middle('Press any key to begin.', 60, (255, 255, 255), win)
         pygame.display.update()
         for event in pygame.event.get():
