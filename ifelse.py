cb82912a36528ea75bbacda5105f92d2c7803b62
Safaraliyev05 <safaraliyevsardor05@gmail.com>
2025-02-01 16:32:01 +0500

    commit

--- ifelse.py
+++ ifelse.py
@@ -1,11 +1,30 @@
-def split_and_join(line):
-    line = line.split()
-    line = "-".join(line)
-    return line
+# def split_and_join(line):
+#     line = line.split()
+#     line = "-".join(line)
+#     return line
+#
+#
+# if __name__ == '__main__':
+#     line = input()
+#     result = split_and_join(line)
+#     print(result)
+#
+print("Salom mening ismim Anvar")
+print("Salom mening ismim Anvar ")
+print("Men PDP Juniorda Python kursida oqiyman")
+print(" Python dunyodagi eng yahshi dasturlash tilaridan biri")
+print(789 + 900)
+print(12 + 8)
+print(22 - 23)
+print(35 / 7)
+print(12 * 38)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
 
+print(15 % 5)
+print(8 ** 2)
 
-if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
-
+print(8 % 19)
+print(90 ** 89)
+print(77 + 84)
