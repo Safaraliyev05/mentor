b43ead3e134b316e7761fbacf960ed1dc8ee6065
Sardor <safaraliyevsardor05@gmail.com>
2025-02-07 20:12:51 +0500

    commit

--- py_gam4.py
+++ py_gam4.py
@@ -1,21 +1,36 @@
-import pygame
-pygame.init()
-window = pygame.display.set_mode((750, 750))
+# import pygame
+# pygame.init()
+# window = pygame.display.set_mode((750, 750))
+#
+# font = pygame.font.SysFont('Tahoma', 40, 'bold')
+#
+# running = True
+# while running:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+#
+#     window.fill('black')
+#     score = 0
+#     text = font.render(f'{score}', True, 'yellow')
+#
+#     window.blit(text, (300, 50))
+#
+#     pygame.display.update()
+#
+# pygame.quit()
 
-font = pygame.font.SysFont('Tahoma', 40, 'bold')
+# a = int(input('Sonni kiriting'))
+# if a > 0:
+#     print(f"{a} soni musbat")
+# else:
+#     print(f"{a} soni manfiy")
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+# a = int(input('1-sonni kiriting: '))
+# b = int(input('2-sonni kiriting: '))
+# print(type(a - b))
+# string - matn " "
+# integer - butun son 1 2 3 4
 
-    window.fill('black')
-    score = 0
-    text = font.render(f'{score}', True, 'yellow')
-
-    window.blit(text, (300, 50))
-
-    pygame.display.update()
-
-pygame.quit()
+name = input('Ismingizni kiritng: ')
+print(type(name))
